701799e413c6da7a413a2da7c39049adc284d81c
Ignacio Cantando Flego <ignacanflego@gmail.com>
2026-05-24 23:26:00 -0300

    redribbon

--- features/items/passive_items/effects/red_ribbon_item_effect.gd
+++ features/items/passive_items/effects/red_ribbon_item_effect.gd
@@ -1,16 +1,12 @@
 extends PassiveItemEffect
-class_name CopperLensItemEffect
+class_name RedRibbonItemEffect
 
-const BASE_BONUS := 2.0
-
-var applied_this_resolution := false
+const BASE_BONUS := 4.0
 
 func on_item_added() -> void:
-	_connect_signal_safe(BookEventBus.bet_pre_resolve, _on_bet_pre_resolve)
 	_connect_signal_safe(BookEventBus.bet_resolved, _on_bet_resolved)
 
 func on_item_removed() -> void:
-	_disconnect_signal_safe(BookEventBus.bet_pre_resolve, _on_bet_pre_resolve)
 	_disconnect_signal_safe(BookEventBus.bet_resolved, _on_bet_resolved)
 
 func on_item_use(roulette_controller: RouletteController) -> void:
@@ -19,13 +15,11 @@ func on_item_use(roulette_controller: RouletteController) -> void:
 	animate.emit()
 	roulette_controller.add_base(BASE_BONUS)
 
-func _on_bet_pre_resolve(_roulette_controller: RouletteController) -> void:
-	applied_this_resolution = false
-
 func _on_bet_resolved(roulette_controller: RouletteController) -> void:
-	if applied_this_resolution:
+	if roulette_controller == null or roulette_controller.winner_betfield_model == null:
 		return
-	applied_this_resolution = true
+	if roulette_controller.winner_betfield_model.color != Constants.BET_FIELD_COLOR.RED:
+		return
 	on_signal_triggered(roulette_controller)
 
 func _connect_signal_safe(signal_ref: Signal, callable: Callable) -> void:
